d9bf2769f8cf84b0ef57cc545afaed577ac80ca2
Daniel Lim <hbt@markhamboards.com>
2016-08-10 17:07:40 -0400

    overall updates
    + updates

--- inc/footer.php
+++ inc/footer.php
@@ -1,14 +1,36 @@
-<div class="container">
-  <div class="row">
-    <div class="col-lg-8 col-lg-offset-2">
-      <div class="modal-body">
-        <!-- Project Details Go Here -->
-        <h2>Video Marketing</h2>
-          <div style="">
-            <iframe src="http://profoundideation.com/videos/portfolio/"></iframe>
-          </div>
-        <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+<footer>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <span class="copyright">
+					<a href="http://ProfoundIdeation.com/inc/" style="text-decoration:none" rel="contact met co-worker" title="Profound Ideation Inc. Web Development & SEO Toronto" alt="Profound Ideation Inc. SEO & Web Dev. Toronto"><img src="http://torontoseokeywords.com/wp-content/uploads/2012/10/ProfoundIdeation-Logo.png" title="Profound Ideation Inc. Web Development & SEO Toronto" alt="Profound Ideation Inc. SEO & Web Dev. Toronto" width="15px" height="15px" /></a>	Profound Ideation Inc. Digital Marketing Agency<br/><pre style="text-align: left;">Copyright &copy; 2016 | All Rights Reserved.</pre></span>
+      </div>
+      <div class="col-md-4">
+        <ul class="list-inline social-buttons">
+          <li>
+            <a href="http://bit.ly/ProfoundIDtwitter" target="_blank" alt="Twitter: Profound Ideation Inc." title="Twitter: Profound Ideation Inc."><i class="fa fa-twitter"></i></a>
+          </li>
+          <li>
+            <a href="http://on.fb.me/ProfoundIdeation" target="_blank" alt="FaceBook: Profound Ideation Inc." title="FaceBook: Profound Ideation Inc."><i class="fa fa-facebook"></i></a>
+          </li>
+          <li>
+            <a href="http://linkd.in/profoundIdeationInc" target="_blank" alt="Linkedin: Profound Ideation Inc." title="Linkedin: Profound Ideation Inc."><i class="fa fa-linkedin"></i></a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-md-4">
+        <ul class="list-inline quicklinks" style="color: #102020 !important; font-weight: 400;">
+          <li>
+            <i class="icon-comments-alt"></i> <a href="http://profoundideation.com/inc/privacy-policy/" style="color: #102020 !important; font-weight: 400;">Privacy Policy</a>
+          </li>
+          <li>
+            <i class="icon-comments"></i> <a href="http://profoundideation.com/inc/terms-of-use/" style="color: #102020 !important; font-weight: 400;">Terms of Use</a>
+          </li>
+          <li>
+            <i class="icon-comments-alt"></i> <a href="http://profoundideation.com/inc/terms-conditions/" style="color: #102020 !important; font-weight: 400;">Terms & Conditions</a>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
-</div>
+</footer>

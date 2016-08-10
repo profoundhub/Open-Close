9dfd714f3c7a2e722cee5871cae63ed735913d82
Daniel Lim <hbt@markhamboards.com>
2016-08-10 16:57:07 -0400

    add inc.
    inc ++

---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+<div class="container">
+  <div class="row">
+    <div class="col-lg-8 col-lg-offset-2">
+      <div class="modal-body">
+        <!-- Project Details Go Here -->
+        <h2>Video Marketing</h2>
+          <div style="">
+            <iframe src="http://profoundideation.com/videos/portfolio/"></iframe>
+          </div>
+        <button type="button" class="btn btn-primary" data-dismiss="modal"><i class="fa fa-times"></i> Close Project</button>
+      </div>
+    </div>
+  </div>
+</div>
